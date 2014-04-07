5a96a1eaa1ca3cfd7ada9933d9ec190ddf005768
hitchcme <hitchcme@gmail.com>
2014-04-07 15:14:47 -0600

    Initial Commit

---
+++ GirinoScope/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GirinoScope
+//
+//  Created by Hitchcock, Michael, E. on 4/7/14.
+//  Copyright (c) 2014 Hitch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
