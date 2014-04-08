7c50c228016010c65a4fe4651d23cfd2fef5e98f
hitchcme <hitchcme@gmail.com>
2014-04-08 13:43:14 -0600

    get_presets returns array of integers

--- GirinoScope/Serial.h
+++ GirinoScope/Serial.h
@@ -14,7 +14,7 @@ char *outToArduino = new char[255];
 
 
 // Get Presets Function
-void get_presets(int fd) {
+long int * get_presets(int fd) {
     
     tcflush(fd, TCIFLUSH);
 
@@ -28,12 +28,46 @@ void get_presets(int fd) {
 
     read(fd,inFromArduino,109);
     
-//    char* presets = strtok(inFromArduino, ":");
-//    char* presets1 = strtok(presets, ":");
+    char* presets_ch = strtok(inFromArduino, ":\n");
+
+        presets_ch = strtok(NULL, ":\n");
+    long int buffsiz=atol(presets_ch);
+        presets_ch = strtok(NULL, ":\n");
+        presets_ch = strtok(NULL, ":\n");
+    long int baudrat=atol(presets_ch);
+        presets_ch = strtok(NULL, ":\n");
+        presets_ch = strtok(NULL, ":\n");
+    long int waitdur=atol(presets_ch);
+        presets_ch = strtok(NULL, ":\n");
+        presets_ch = strtok(NULL, ":\n");
+    long int prescal=atol(presets_ch);
+        presets_ch = strtok(NULL, ":\n");
+        presets_ch = strtok(NULL, ":\n");
+    long int trigger=atol(presets_ch);
+        presets_ch = strtok(NULL, ":\n");
+        presets_ch = strtok(NULL, ":\n");
+    long int thresho=atol(presets_ch);
+
+    long int presets[6];
+    presets[0]=buffsiz;
+    presets[1]=baudrat;
+    presets[2]=waitdur;
+    presets[3]=prescal;
+    presets[4]=trigger;
+    presets[5]=thresho;
     
+    return presets;
     
-//    std::cout<<presets[2]<<std::endl;
-    std::cout<<inFromArduino<<std::endl<<std::flush;
+//    std::cout<<"buffersize: "<<buffsiz<<std::endl;
+//    std::cout<<"baudrate:   "<<baudrat<<std::endl;
+//    std::cout<<"waitdur:    "<<waitdur<<std::endl;
+//    std::cout<<"Prescaler   "<<prescal<<std::endl;
+//    std::cout<<"Trigger     "<<trigger<<std::endl;
+//    std::cout<<"Threshold   "<<thresho<<std::endl;
+
+
+//    std::cout<<presets1[1,2]<<std::endl;
+//    std::cout<<inFromArduino<<std::endl<<std::flush;
 }
 
 #endif
