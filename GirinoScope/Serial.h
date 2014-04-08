bc0b6ff28a918840085c5519cb3660e311e676ce
hitchcme <hitchcme@gmail.com>
2014-04-08 10:59:19 -0600

    Update Serial.h
    fuck

--- GirinoScope/Serial.h
+++ GirinoScope/Serial.h
@@ -1,1 +1,14 @@
-a
+//
+//  Serial.h
+//  GirinoScope
+//
+//  Created by Hitchcock, Michael, E. on 4/8/14.
+//  Copyright (c) 2014 Hitch. All rights reserved.
+//
+
+#ifndef GirinoScope_Serial_h
+#define GirinoScope_Serial_h
+
+
+
+#endif
